4c064580987cefb6aacf05a526ceff8b44046698
dishant3896 <dish3896@gmail.com>
2017-07-13 11:24:20 +0530

    implement code

--- Currency/Program.cs
+++ Currency/Program.cs
@@ -10,6 +10,21 @@ namespace Currency
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter Amount");
+            string amnt = Console.ReadLine();
+
+            //char last=amnt[amnt.Length - 1];
+            //int numericValue;
+
+            if (char.IsDigit(amnt[amnt.Length - 1]))
+            {
+                Console.WriteLine(amnt.Remove(0, 1));
+            }
+            else
+            {
+                Console.WriteLine(amnt.Remove(amnt.Length - 1, 1));
+            }
+            Console.ReadLine();
         }
     }
 }
